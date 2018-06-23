a5fc56725838ea1ea54cb520f2dd8bd72a1e6d85
marianchelmus <chelmus.marian@gmail.com>
2018-06-23 14:28:26 +0300

    update config-staic-ip.sh for ubuntu tests

--- bash/config-static-ip.sh
+++ bash/config-static-ip.sh
@@ -5,31 +5,37 @@ OS=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }' | sed 's/"/
 defInterface=$(ip addr show | awk '/inet.*brd/{print $NF}' | head -n 1)
 defIP=$(ip -f inet a show $defInterface | grep inet | awk '{ print $2 }' | cut -d / -f1)
 defNetmask=$(ip -f inet a show eth0 | grep inet | awk '{ print $2 }' | rev | cut -d / -f1 | rev)
+ubuntuNetmask=$(ifconfig $defInterface | sed -rn '2s/ .*:(.*)$/\1/p')
 gateway=$(ip r | grep default | awk '{print $3}')
-#netcfg="/etc/sysconfig/network-scripts/ifcfg-$defInterface"
-netcfg="./test-eth0"
+#netcfgCentos="/etc/sysconfig/network-scripts/ifcg-$defInterface"
+netcfgCentos="/ansible/bash/test-eth0"
+#netcfgUbuntu="/etc/network/interfaces
+netcfUbuntu="/root/interfaces"
+
 #echo vars
 echo "---------------------------------------------------------------"
 echo -e "Operating system is: " '\033[1m'$OS'\033[0m'
 echo -e "Default interface is: " '\033[1m'$defInterface'\033[0m'
 echo -e "Main IP is: " '\033[1m'$defIP'\033[0m'
 echo -e "Netmask is: " '\033[1m' $defNetmask'\033[0m'
-echo -e "Network configuration file located at: "'\033[1m' $netcfg'\033[0m'
 if [ $OS  == 'centos' ]
 then
 	echo "---------------------------------------------------------------"
-	if grep -q dhcp $netcfg
+	echo "Configuring the interface for $OS"
+	echo -e "Network configuration file located at: "'\033[1m' $netcfgCentos'\033[0m'
+	if grep -q dhcp $netcfgCentos
 	then
-		sed -i 's/dhcp/static/g' $netcfg
-		sed -i "s/.*BOOTPROTO.*/&\nIPADDR=\"$defIP\"/" $netcfg
-		sed -i "s/.*IPADDR.*/&\nPREFIX=\"$defNetmask\"/" $netcfg
-		sed -i "s/.*PREFIX.*/&\nGATEWAY=\"$gateway\"/" $netcfg
-		sed -i "s/.*GATEWAY.*/&\nDNS1=\"8.8.8.8\"/" $netcfg
-		sed -i "s/.*GATEWAY.*/&\nDNS1=\"8.8.4.4\"/" $netcfg
+		sed -i 's/dhcp/static/g' $netcfgCentos
+		sed -i "s/.*BOOTPROTO.*/&\nIPADDR=\"$defIP\"/" $netcfgCentos
+		sed -i "s/.*IPADDR.*/&\nPREFIX=\"$defNetmask\"/" $netcfgCentos
+		sed -i "s/.*PREFIX.*/&\nGATEWAY=\"$gateway\"/" $netcfgCentos
+		sed -i "s/.*GATEWAY.*/&\nDNS1=\"8.8.8.8\"/" $netcfgCentos
+		sed -i "s/.*GATEWAY.*/&\nDNS2=\"8.8.4.4\"/" $netcfgCentos
 		echo "Interface was configured:"
- 	        cat $netcfg	
+ 	        cat $netcfgCentos	
 	else
 		echo "Interface is already configured"
+		cat $netcfgCentos
 	fi 
 	echo ""
 	echo "---------------------------------------------------------------"
@@ -37,9 +43,22 @@ then
 
 elif [ $OS == 'ubuntu' ]
 then
-	echo "todo"
-
+	echo "---------------------------------------------------------------"
+        echo "Configuring the interface for $OS"
+        echo -e "Network configuration file located at: "'\033[1m' $netcfgUbuntu'\033[0m'
+	if grep -q dhcp $netcfgUbuntu
+	then
+		sed -i 's/dhcp/static/g' $netcfgUbuntu
+		echo "        address $defIP"
+		echo "        netmask $ubuntuNetmask"
+		echo "        gateway $gateway"
+		echo "        dbs-nameservers 8.8.8.8 8.8.4.4"
+	else
+		echo "Interface was already configured"
+		cat $netcfgUbuntu
 else
 	echo "OS is not Ubuntu or Centos"
 	exit 4
 fi
+
+
